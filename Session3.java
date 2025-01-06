d1dd27e48eaf749b6160d4f85f7260e3eb93e25b
05ARCHANA <akuthotaarchana2@gail.com>
2025-01-06 13:57:57 +0530

    first commit

---
+++ Session3.java
@@ -0,0 +1,12 @@
+public class Session3{
+    public static void main(String[] args) {
+        String str="hello";
+        String str1="mountain";
+        int len = str.length();
+        System.out.println(str.length());
+        System.out.println(str.charAt(len-1));
+        System.out.println(str.charAt(len-2));
+        System.out.println(str.charAt(len-3));
+
+    }
+}
